5248071cf79021d962d80341c2063af7f04f7919
Subhranil Dutta <monika9851dutta@gmail.com>
2026-02-04 21:23:35 +0530

    Create Q2.py

---
+++ Day 1/Q2.py
@@ -0,0 +1,9 @@
+# Q2. 2️⃣ Take two numbers → print:
+## Sum, Difference, Product
+
+a=int(input('Enter first number:'))
+b=int(input('Enter second number:'))
+total=a+b
+difference=a-b
+product=a*b
+print(f"Sum is {total} and difference is {difference} and product is {product}")
